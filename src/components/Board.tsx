4bf9ce15c651a1db6cf40a1749116f072ac98c54
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-07-25 14:06:50 +0200

    Review board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,24 @@
 import Cell from "@/components/Cell";
+import Confetti from "react-confetti";
 
-const Board = ({cells, onClick, isGameOver} : { cells: number | null, onClick: (i: number) => void, isGameOver: boolean }) => {
+const emptyBoard = Array(9).fill(0);
+
+const Board = ({cells = emptyBoard, onClick, isGameOver}: {
+    cells: number[],
+    onClick: (i: number) => void,
+    isGameOver: boolean
+}) => {
     return (
-        <div className="grid grid-cols-3 gap-4 p-5 rounded bg-slate-400 mb-10">
-            {cells.map((value, i) => (
-                <Cell key={i} value={value} onClick={() => onClick(i)} isGameOver={isGameOver} />
-            ))}
-        </div>
+        <>
+            <div className="grid grid-cols-3 gap-4 p-5 rounded bg-slate-400 mb-10">
+                {cells.map((value, i) => (
+                    <Cell key={i} value={value} onClick={() => onClick(i)} isGameOver={isGameOver}/>
+                ))}
+            </div>
+            {isGameOver &&
+                <Confetti />
+            }
+        </>
     )
 }
 
